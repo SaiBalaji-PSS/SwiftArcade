4ca07783bd58fccd9be8ff9c543dfaea86456d8b
Sai Balaji K <ksbalaji2022@gmail.com>
2022-11-20 20:28:20 +0530

    NONE

--- Sources/SwiftArcade/Model/Model.swift
+++ Sources/SwiftArcade/Model/Model.swift
@@ -8,23 +8,23 @@
 import Foundation
 
 public struct Consoles: Codable{
-    let console: [[Console]]
+    public let console: [[Console]]
 }
 public struct Console: Codable{
-    let ID: String
-    let Name: String
+    public let ID: String
+    public let Name: String
 }
 
 
 public struct TopUsers: Codable {
-    let top10: Top10
+    public let top10: Top10
 }
 
 // MARK: - Top10
 public struct Top10: Codable {
-    let place1, place2, place3, place4: Place
-    let place5, place6, place7, place8: Place
-    let place9, place10: Place
+    public let place1, place2, place3, place4: Place
+    public let place5, place6, place7, place8: Place
+    public let place9, place10: Place
 
     enum CodingKeys: String, CodingKey {
         case place1 = "place_1"
@@ -42,55 +42,55 @@ public struct Top10: Codable {
 
 
 public struct Place: Codable {
-    let user, score, trueratio: String
+    public let user, score, trueratio: String
 }
 
 //MARK: - GAME LIST
 public struct GamesList: Codable{
-    let game: [[Game]]
+    public let game: [[Game]]
 }
 public struct Game: Codable{
-    let Title: String
-    let ID: String
-    let ConsoleID: String
-    let ConsoleName: String
-    let ImageIcon: String
-    let NumAchievements: Int
-    let NumLeaderboards: Int
-    let Points: Int
-    let DataModified: String?
-    let ForumTopicID: Int?
+    public let Title: String
+    public   let ID: String
+    public   let ConsoleID: String
+    public  let ConsoleName: String
+    public  let ImageIcon: String
+    public let NumAchievements: Int
+    public let NumLeaderboards: Int
+    public  let Points: Int
+    public let DataModified: String?
+    public let ForumTopicID: Int?
 }
 
 
 //MARK: - GAME INFO EXTENDED
 
 public struct GameInfo: Codable{
-    let ID: Int
-    let Title: String
-    let ConsoleID: Int
-    let ImageIcon: String
-    let ImageTitle: String
-    let ImageIngame: String
-    let ImageBoxArt: String
-    let Publisher: String
-    let Developer: String
-    let Genre: String
-    let Released: String
-    let ConsoleName: String
-    let NumAchievements: Int
-    let NumDistinctPlayersCasual: String
-    let NumDistinctPlayersHardcore: String
-    let Achievements: [String: Trophies]
+    public  let ID: Int
+    public  let Title: String
+    public  let ConsoleID: Int
+    public let ImageIcon: String
+    public  let ImageTitle: String
+    public  let ImageIngame: String
+    public  let ImageBoxArt: String
+    public  let Publisher: String
+    public  let Developer: String
+    public  let Genre: String
+    public  let Released: String
+    public let ConsoleName: String
+    public let NumAchievements: Int
+    public let NumDistinctPlayersCasual: String
+    public  let NumDistinctPlayersHardcore: String
+    public  let Achievements: [String: Trophies]
 }
 
 public struct Trophies: Codable{
-    let Title: String
-    let Description: String
-    let Points: String
-    let DateEarned: String?
-    let NumAwarded: String?
-    let NumAwardedHardcore: String?
+    public  let Title: String
+    public  let Description: String
+    public let Points: String
+    public let DateEarned: String?
+    public let NumAwarded: String?
+    public let NumAwardedHardcore: String?
 }
 
 //MARK: - USER RECENT
@@ -103,16 +103,16 @@ public struct Trophies: Codable{
 
 // MARK: - Welcome
 public struct Recents: Codable {
-    let recent: [[Recent]]
+    public let recent: [[Recent]]
 }
 
 // MARK: - Recent
 public struct Recent: Codable {
-    let gameID, consoleID, consoleName, title: String
-    let imageIcon, lastPlayed: String
-    let numPossibleAchievements, possibleScore: Achieved
-    let numAchieved, scoreAchieved: Achieved
-    let numAchievedHardcore, scoreAchievedHardcore: Achieved
+   public let gameID, consoleID, consoleName, title: String
+    public let imageIcon, lastPlayed: String
+    public let numPossibleAchievements, possibleScore: Achieved
+    public let numAchieved, scoreAchieved: Achieved
+    public let numAchievedHardcore, scoreAchievedHardcore: Achieved
 
     enum CodingKeys: String, CodingKey {
         case gameID = "GameID"
@@ -130,11 +130,11 @@ public struct Recent: Codable {
     }
 }
 
-enum Achieved: Codable {
+public enum Achieved: Codable {
     case integer(Int)
     case string(String)
 
-    init(from decoder: Decoder) throws {
+    public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         if let x = try? container.decode(Int.self) {
             self = .integer(x)
@@ -147,7 +147,7 @@ enum Achieved: Codable {
         throw DecodingError.typeMismatch(Achieved.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for Achieved"))
     }
     
-    var intValue: Int? {
+    public var intValue: Int? {
             switch self {
             case .integer(let value): return value
             case .string(let value): return Int(value)
